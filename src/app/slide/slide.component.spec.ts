92d310f99e24ad379b9a6d6dc86c98b867f8967b
Alexander Wiebe <alexanderjwiebe@gmail.com>
2022-04-15 14:36:11 -0600

    creating a generic slide component

---
+++ src/app/slide/slide.component.spec.ts
@@ -0,0 +1,11 @@
+import { ComponentFixture } from '@angular/core/testing';
+import { SlideComponent } from './slide.component';
+
+describe('SlideComponent', () => {
+  let component: SlideComponent;
+  let fixture: ComponentFixture<SlideComponent>;
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/slide/slide.component.ts
@@ -0,0 +1,6 @@
+export class SlideComponent {
+  hoodyCost = 18.99;
+  markdown = `\`\`\`
+\`\`\``;
+  constructor() {}
+}
